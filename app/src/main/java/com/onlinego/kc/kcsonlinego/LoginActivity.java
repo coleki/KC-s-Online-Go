a0c40809e36bf07bc042a05ad03f78a11ad950e4
coleki <coleki@gmail.com>
2016-04-19 12:07:42 -0700

    first commit
    first commit

---
+++ app/src/main/java/com/onlinego/kc/kcsonlinego/LoginActivity.java
@@ -0,0 +1,67 @@
+package com.onlinego.kc.kcsonlinego;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+
+/**
+ * https://futurestud.io/blog/oauth-2-on-android-with-retrofit
+ * 4/19/2016
+ */
+public class LoginActivity extends Activity {
+
+    // you should either define client id and secret as constants or in string resources
+    
+    private final String clientId = "REDACTED";
+    private final String clientSecret = "REDACTED";
+    private final String redirectUri = "https://online-go.com/oauth2/access_token";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button) findViewById(R.id.loginbutton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse(ServiceGenerator.API_BASE_URL + "/login" + "?client_id=" + clientId + "&redirect_uri=" + redirectUri));
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // the intent filter defined in AndroidManifest will handle the return from ACTION_VIEW intent
+        Uri uri = getIntent().getData();
+        if (uri != null && uri.toString().startsWith(redirectUri)) {
+            // use the parameter your API exposes for the code (mostly it's "code")
+            String code = uri.getQueryParameter("code");
+            if (code != null) {
+                // get access token
+                LoginService loginService = ServiceGenerator.createService(LoginService.class, clientId, clientSecret);
+                Call<AccessToken> call = loginService.getAccessToken(code, "authorization_code");
+                try {
+                    AccessToken accessToken = call.execute().body();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (uri.getQueryParameter("error") != null) {
+                // show an error message here
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/onlinego/kc/kcsonlinego/LoginService.java
@@ -0,0 +1,16 @@
+package com.onlinego.kc.kcsonlinego;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface LoginService {
+    @FormUrlEncoded
+    @POST("/token")
+    Call<AccessToken> getAccessToken(
+            @Field("code") String code,
+            @Field("grant_type") String grantType);
+
+
+}
